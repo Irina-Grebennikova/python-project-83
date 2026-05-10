54265716417875005a45734f3c9fdb2c398f4fc6
Irina-Grebennikova <girishkaizh@mail.ru>
2026-05-11 01:17:49 +0400

    add a template for the main page

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,7 +1,7 @@
 import os
 
 from dotenv import load_dotenv
-from flask import Flask
+from flask import Flask, render_template
 
 load_dotenv()
 app = Flask(__name__)
@@ -10,4 +10,13 @@ app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 
 @app.route("/")
 def index():
-    return ""
+    return render_template("index.html")
+
+
+if os.getenv("APP_ENV") == "development":
+    from livereload import Server
+
+    server = Server(app.wsgi_app)
+    server.watch("page_analyzer/templates/")
+    server.watch("page_analyzer/static/")
+    server.serve(port=5000, debug=True)
